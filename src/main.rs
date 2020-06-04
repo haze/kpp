8722f15cad0e47987d828f7479349888bdfc7d9f
Haze Booth <isnt@haze.cool>
2020-06-04 02:46:13 -0400

    move to editing url of reddit post, reduce amount requested to paginate for faster initial speeds

--- src/main.rs
+++ src/main.rs
@@ -13,36 +13,26 @@ impl EventHandler for Handler {
     async fn message(
         &self,
         ctx: serenity::prelude::Context,
-        new_message: serenity::model::channel::Message,
+        mut new_message: serenity::model::channel::Message,
     ) {
         if new_message.author.id == OWNER_ID && new_message.content == "kpp" {
-            match new_message.delete(&ctx.http).await {
-                Ok(_) => {
-                    if let Some(redis_connection) = ctx.data.read().await.get::<RedisTypemapKey>() {
-                        let mut conn = redis_connection.lock().await;
-                        if let Ok(Some(pic)) = random_kpop_pic(&mut conn, Some(100)).await {
-                            if let Err(why) = new_message
-                                .channel_id
-                                .send_files(
-                                    &ctx.http,
-                                    vec![serenity::http::AttachmentType::Bytes {
-                                        data: std::borrow::Cow::Owned(pic.data),
-                                        filename: pic.name,
-                                    }],
-                                    |m| m,
-                                )
-                                .await
-                            {
-                                eprintln!("Failed to send kpop pic: {:?}", &why);
-                            }
-                        } else {
-                            eprintln!("Could not get proper kpop pic");
-                        }
-                    } else {
-                        eprintln!("No redis connection available");
+            let found_time = std::time::Instant::now();
+            if let Some(redis_connection) = ctx.data.read().await.get::<RedisTypemapKey>() {
+                let mut conn = redis_connection.lock().await;
+                if let Ok(Some(kpop_pic_url)) = random_kpop_pic(&mut conn, Some(100)).await {
+                    println!("Found kpop pic after {:?}", found_time.elapsed());
+                    if let Err(why) = new_message
+                        .edit(&ctx.http, |m| {
+                            m.content(kpop_pic_url);
+                            m
+                        })
+                        .await
+                    {
+                        eprintln!("Failed to edit message: {:?}", &why);
+                        return;
                     }
+                    println!("Edited message after {:?}", found_time.elapsed());
                 }
-                Err(why) => eprintln!("Err: {:?}", &why),
             }
         }
     }
@@ -117,7 +107,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Ok(())
 }
 
-async fn get_reddit_psots(
+async fn get_reddit_posts(
     subreddit: &str,
     selector: reddit::Selector,
     limit: Option<usize>,
@@ -138,23 +128,12 @@ async fn get_reddit_psots(
         .recv_json().await.map_err(|e| e.into())
 }
 
-async fn download_media(
-    url: &str,
-) -> Result<DownloadedMedia, Box<dyn std::error::Error + Send + Sync>> {
-    Ok(DownloadedMedia {
-        name: url
-            .rsplitn(2, "/")
-            .next()
-            .unwrap_or_else(|| "item")
-            .to_string(),
-        data: surf::get(url).recv_bytes().await?,
-    })
-}
-
 async fn random_kpop_pic(
     redis_connection: &mut redis::aio::Connection,
     max_number_of_pages: Option<usize>,
-) -> Result<Option<DownloadedMedia>, Box<dyn std::error::Error + Send + Sync>> {
+) -> Result<Option<String>, Box<dyn std::error::Error + Send + Sync>> {
+    let start_time = std::time::Instant::now();
+    println!("starting to look for kpop image");
     let max_number_of_pages = max_number_of_pages.unwrap_or_else(|| 100);
     let mut pages_searched = 0;
     let mut after: Option<String> = None;
@@ -164,14 +143,19 @@ async fn random_kpop_pic(
             return Ok(None);
         }
 
-        let root = get_reddit_psots("kpics", reddit::Selector::New, None, after.as_deref()).await?;
+        let root =
+            get_reddit_posts("kpics", reddit::Selector::New, Some(25), after.as_deref()).await?;
+        println!(
+            "got reddit page {} data after {:?}",
+            pages_searched,
+            start_time.elapsed()
+        );
         let new_after = root.data.after.clone();
 
         for random_pic in root.to_set().drain() {
             if !is_already_seen(redis_connection, &random_pic).await? {
                 mark_picture_seen(redis_connection, &random_pic).await?;
-                dbg!(&random_pic);
-                return Ok(Some(download_media(&*random_pic.url).await?));
+                return Ok(Some(random_pic.url));
             }
         }
 
